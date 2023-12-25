fb682597fafecb00322de7d1a2e12bc3e2acf76f
ikomarov <ikomarov_ivan@mail.ru>
2023-12-25 20:22:03 +0300

    Обновление

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import React from "react";
-import Head from "next/head";
-import {CDN_URL} from "@/consts/refs";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,20 +10,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-    <Head>
-        <link rel="icon" href={ `${CDN_URL}favicon.png` } />
-        <title>Meet - Приложение для знакомств в Telegram</title>
-        <meta charSet="utf-8"/>
-        <meta property="og:title" content="Meet - Приложение для знакомств в Telegram" key="title" />
-        <meta property="og:description" name="description" content="Meet - приложение для знакомств, которое помогает людям находить интересные знакомства, дружбу и любовь. Присоединяйтесь к нам сегодня и начните своё путешествие к счастью." key="description" />
-        <meta property="og:image" content="logo.svg" key="image" />
-        <meta property="og:type" content="website" key="type" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:site_name" content="Meet - Приложение для знакомств в Telegram" />
-      </Head>
+      <html lang="en">
+      <head>
+          <title>Meet - Приложение для знакомств в Telegram</title>
+          <link rel="icon" type="image/x-icon" href="https://cdn.lovemeetmet.space/land/public/favicon.png"/>
+          <meta charSet="utf-8"/>
+          <meta property="og:title" content="Meet - Приложение для знакомств в Telegram" key="title"/>
+          <meta property="og:description" name="description"
+                content="Meet - приложение для знакомств, которое помогает людям находить интересные знакомства, дружбу и любовь. Присоединяйтесь к нам сегодня и начните своё путешествие к счастью."
+                key="description"/>
+          <meta property="og:image" content="/logo.svg" key="image"/>
+          <meta property="og:type" content="website" key="type"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:site_name" content="Meet - Приложение для знакомств в Telegram"/>
+      </head>
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
   )
 }
