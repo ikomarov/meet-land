93da0b3ecbe77d62d88928807acb7294d67d53c8
ikomarov <ikomarov_ivan@mail.ru>
2024-01-12 19:32:56 +0300

    Обновление

--- src/components/ArticlesComponent/index.tsx
+++ src/components/ArticlesComponent/index.tsx
@@ -4,39 +4,49 @@ import './index.css'
 interface Article {
     id: number;
     content: string;
+    title: string;
 }
 
 const ArticlesComponent: React.FC = () => {
     const [articles, setArticles] = useState<Article[]>([]);
-    const [currentArticle, setCurrentArticle] = useState<string>('');
-
-    const handleArticleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setCurrentArticle(event.target.value);
-    };
+    const [currentTitle, setCurrentTitle] = useState<string>('');
+    const [currentContent, setCurrentContent] = useState<string>('');
 
     const addArticle = () => {
         const newArticle = {
             id: articles.length + 1,
-            content: currentArticle
+            title: currentTitle,
+            content: currentContent
         };
         setArticles([...articles, newArticle]);
-        setCurrentArticle('');
+        setCurrentTitle('');
+        setCurrentContent('');
     };
 
     return (
         <div className="articles-container">
-              <textarea
-                  value={currentArticle}
-                  onChange={handleArticleChange}
-                  placeholder="Введите HTML статьи..."
-                  rows={10}
-                  cols={50}
-              />
+            <input
+                className="title-input"
+                type="text"
+                value={currentTitle}
+                onChange={(e) => setCurrentTitle(e.target.value)}
+                placeholder="Заголовок статьи"
+            />
+            <textarea
+                value={currentContent}
+                onChange={(e) => setCurrentContent(e.target.value)}
+                placeholder="Введите HTML статьи..."
+                rows={10}
+                cols={50}
+            />
             <button onClick={addArticle}>Добавить</button>
             <div>
                 <h2>Список статей</h2>
                 {articles.map(article => (
-                    <div key={article.id} dangerouslySetInnerHTML={{ __html: article.content }} />
+                    <div key={article.id}>
+                        <h3>{article.title}</h3>
+                        <div dangerouslySetInnerHTML={{ __html: article.content }} />
+                    </div>
                 ))}
             </div>
         </div>
