ca73d91f6037ddf69e6c1a68ed1c74c9d00d537c
ikomarov <ikomarov_ivan@mail.ru>
2024-01-13 17:36:33 +0300

    Обновление

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,15 @@
+const fetchBlogPosts = async () => {
+    // Замените этот URL на URL вашего API
+    const res = await fetch('https://lovemeetmet.space/api/blogs');
+    const {blogs} = await res.json();
+    return blogs.map(post => `/blog/${post.slug}`);
+};
+
 module.exports = {
     siteUrl: 'https://lovemeetmet.space',
     generateRobotsTxt: true,
+    additionalPaths: async (config) => {
+        const blogPaths = await fetchBlogPosts();
+        return blogPaths.map(path => ({ loc: path }));
+    },
 };
